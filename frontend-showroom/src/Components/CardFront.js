5d1932cb7836da7576706bed451069ddc929da6b
qudroturkey <reesemb1@gmail.com>
2020-04-14 17:09:08 -0500

    add containers to cards

--- frontend-showroom/src/Components/CardFront.js
+++ frontend-showroom/src/Components/CardFront.js
@@ -1,24 +1,23 @@
 import React from 'react'
 import { Card, Image, Rating, Icon } from 'semantic-ui-react'
-// const poster_url = `https://image.tmdb.org/t/p/w500/`
+
+const poster_url = `https://image.tmdb.org/t/p/w780/`
 
 const CardFront = (props) => 
-  <Card.Group>
-    <Card >
-      <Card.Content>
-        <Image src={`https://image.tmdb.org/t/p/w780/${props.movie.poster_path}`} onClick={props.handleClick}/>
-        <Card.Header>{props.movie.title}</Card.Header>
-        <Card.Meta>
-          released: {props.movie.release_date}
-        </Card.Meta>
-          <Icon onClick={console.log('save to user func goes here')} name='save'/>
-      </Card.Content>
-      <Card.Content extra>
-        <a>
-          <Rating maxRating={5} defaultRating={props.movie.vote_average/2} disabled/>         
-        </a>      
-      </Card.Content>
-    </Card>
-  </Card.Group>
+  <Card fluid>
+    <Card.Content>
+      <Image src={`${poster_url}${props.movie.poster_path}`} onClick={props.handleClick}  />
+      <Card.Header>{props.movie.title}</Card.Header>
+      <Card.Meta>
+        {props.movie.release_date.slice(0,4)}
+      </Card.Meta>
+    </Card.Content>
+    <Card.Content extra>
+      <a>
+        <Rating maxRating={5} defaultRating={props.movie.vote_average/2} disabled/>         
+        <Icon onClick={console.log('save to user func goes here')} name='save'/>
+      </a>      
+    </Card.Content>
+  </Card>
 
 export default CardFront
