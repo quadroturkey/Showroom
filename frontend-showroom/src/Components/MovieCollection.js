02f63186408ab7a90254a8d78b217d9721243137
qudroturkey <reesemb1@gmail.com>
2020-04-14 14:04:39 -0500

    horizontal scroll works

--- frontend-showroom/src/Components/MovieCollection.js
+++ frontend-showroom/src/Components/MovieCollection.js
@@ -1,17 +1,28 @@
 import React from 'react'
-import { Grid } from 'semantic-ui-react'
-import MoiveCard from './MovieCard'
-
-const MoiveCollection = (props) => (
-  <Grid divided='vertically' padded>
-    <Grid.Row columns={4}>
-      {props.movies.map(movie => (
-        <Grid.Column width={3}>
-          <MoiveCard movie={movie} key={movie.id} />
-        </Grid.Column>
-      ))}
-    </Grid.Row>
-  </Grid>
-)
+import MovieCard from './MovieCard'
+import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
+import 'pure-react-carousel/dist/react-carousel.es.css';
+let i = 0
+ 
+class MoiveCollection extends React.Component {
+  render() {
+    return (
+      <CarouselProvider
+        visibleSlides={5}
+        step={3}
+        naturalSlideWidth={245}
+        naturalSlideHeight={420}
+        totalSlides={this.props.movies.length}
+        infinite
+      >
+      <ButtonBack>Back</ButtonBack>
+      <ButtonNext>Next</ButtonNext>
+        <Slider>
+          {this.props.movies.map(movie => <Slide index={i++}><MovieCard movie={movie} key={movie.id}/></Slide>)}
+        </Slider>
+      </CarouselProvider>
+    );
+  }
+}
 
 export default MoiveCollection
